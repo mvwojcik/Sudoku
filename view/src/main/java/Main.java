471cd7e29962cd0a19b5b1fb77a5e122780f4cf9
MateuszWojcik <mateuszw.1998@wp.pl>
2019-04-16 11:15:10 +0200

    fxml, controller, stylesheet

--- view/src/main/java/Main.java
+++ view/src/main/java/Main.java
@@ -1,2 +1,19 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxmls/SudokuBoard.fxml"));
+        BorderPane borderPane = loader.load();
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Sudoku");
+        primaryStage.show();
+
+    }
 }
--- view/src/main/java/controllers/SudokuBoardController.java
+++ view/src/main/java/controllers/SudokuBoardController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class SudokuBoardController {
+
+  Button prevButton;
+    TextField textField;
+  @FXML private GridPane gridPane;
+
+  @FXML
+  void clickOnAction(ActionEvent event) {
+    if (prevButton != null) {
+        prevButton.setText(textField.getText());
+      prevButton.setGraphic(null);
+    }
+    textField = new TextField();
+    ((Button) (event.getSource())).setGraphic(textField);
+    ((Button) (event.getSource())).setText(textField.getText());
+    prevButton = ((Button) (event.getSource()));
+  }
 }
