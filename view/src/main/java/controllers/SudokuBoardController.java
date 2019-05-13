f715fc43bf823ca974718c21be6c0c6577c7e9ed
student FTIMS <student@lab.it.p.lodz.pl>
2019-05-13 09:27:37 +0200

    some jfx features

--- view/src/main/java/controllers/SudokuBoardController.java
+++ view/src/main/java/controllers/SudokuBoardController.java
@@ -1,26 +1,60 @@
 package controllers;
 
+import algorithms.BackTrackingSudokuSolver;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import model.SudokuBoard;
 
 public class SudokuBoardController {
 
-  Button prevButton;
+    Button prevButton;
     TextField textField;
-  @FXML private GridPane gridPane;
+    TextField previousTextField;
+    SudokuBoard sudokuBoard;
+    private BackTrackingSudokuSolver sudokuBoard1Solver;
 
-  @FXML
-  void clickOnAction(ActionEvent event) {
-    if (prevButton != null) {
-        prevButton.setText(textField.getText());
-      prevButton.setGraphic(null);
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private void initialize() {
+        sudokuBoard1Solver = new BackTrackingSudokuSolver();
+        sudokuBoard = new SudokuBoard();
+        sudokuBoard1Solver.solve(sudokuBoard);
+        for (int i = 0; i < 9 ; i++) {
+            for (int j = 0; j <9 ; j++) {
+                
+            }
+
+        }
     }
-    textField = new TextField();
-    ((Button) (event.getSource())).setGraphic(textField);
-    ((Button) (event.getSource())).setText(textField.getText());
-    prevButton = ((Button) (event.getSource()));
-  }
+
+    @FXML
+    void clickOnAction(ActionEvent event) {
+        if (previousTextField != null && !validate(previousTextField.getText())) {
+            previousTextField.setText("");
+        }
+        if (prevButton != null) {
+            prevButton.setText(textField.getText());
+            prevButton.setGraphic(null);
+        }
+        textField = new TextField();
+        ((Button) (event.getSource())).setGraphic(textField);
+        ((Button) (event.getSource())).setText(textField.getText());
+        prevButton = ((Button) (event.getSource()));
+        previousTextField = textField;
+    }
+
+
+    private boolean validate(String jazda) {
+
+        if (jazda.matches("[1-9]")) {
+            return true;
+        }
+        return false;
+    }
 }
